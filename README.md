f073e1fd558c6fb1f0ba3971ac9293e592c92a5c
Jorge Freitas <jorge_freitas@discente.ufg.br>
2023-08-03 22:25:40 -0300

    Alterações para a tarefa

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # Pub-Sub-Basics-with-ZeroMQ
 
 This is a very simple pub-sub app implemented with ZeroMQ. Use it as an example for the pub-sub assignment (topic-based chat system).
+alterações para tarefa
--- publisher.py
+++ publisher.py
@@ -6,6 +6,6 @@ s = context.socket(zmq.PUB)        # create a publisher socket
 p = "tcp://"+ HOST +":"+ PORT      # how and where to communicate
 s.bind(p)                          # bind socket to the address
 while True:
-	time.sleep(5)                    # wait every 5 seconds
+	time.sleep(15)                    # wait every 5 seconds
 	msg = str.encode("TIME " + time.asctime())
 	s.send(msg) # publish the current time
